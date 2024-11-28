dc996e86f3a96c322e087b44c3d73f2bab1ca605
leopold rouxdecorzent <leopold.rouxdecorzent@saintmichelannecy.fr>
2024-11-28 10:53:08 +0100

    Rudimentaire

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-wvnfqlkJwodsdss
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Erreur';
+    }
+}
